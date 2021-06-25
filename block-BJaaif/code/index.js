7c7b1675fba41a6a6f73d587c6ec302f34d52dcc
bitopan987 <bitopanxon1@gmail.com>
2021-06-25 21:44:11 +0530

    Refactor

--- block-BJaaif/code/index.js
+++ block-BJaaif/code/index.js
@@ -24,7 +24,7 @@ console.log(vicky.age); // -> Logs 24
 /*** CHALLENGE 1 of 3 ***/
 
 var personStore = {
-  greet: function () {
+  greet() {
     console.log("hello");
   },
 };
@@ -49,8 +49,10 @@ console.log(sandra.age); //-> Logs 26
 sandra.greet(); //-> Logs 'hello'
 
 /*** CHALLENGE 3 of 3 ***/
-
-// sandra.introduce(); // -> Logs 'Hi, my name is Sandra'
+personStore.introduce = function () {
+  console.log(`Hi, my name is ${this.name}`);
+};
+sandra.introduce(); // -> Logs 'Hi, my name is Sandra'
 
 /****************************************************************
                     USING THE 'NEW' KEYWORD
@@ -59,30 +61,37 @@ sandra.greet(); //-> Logs 'hello'
 /*** CHALLENGE 1 of 3 ***/
 
 function PersonConstructor() {
-  // add code here
+  this.greet = function () {
+    console.log("hello");
+  };
+  this.introduce = function () {
+    console.log(`Hi, my name is ${this.name}`);
+  };
 }
 
 // /********* Uncomment this line to test your work! *********/
 var simon = new PersonConstructor();
-// simon.greet(); // -> Logs 'hello'
+simon.greet(); // -> Logs 'hello'
 
 /*** CHALLENGE 2 of 3 ***/
 
-function personFromConstructor(name, age) {
-  // add code here
+function PersonFromConstructor(name, age) {
+  let obj = new PersonConstructor();
+  obj.name = name;
+  obj.age = age;
+  return obj;
 }
 
-var mike = personFromConstructor("Mike", 30);
+var mike = new PersonFromConstructor("Mike", 30);
 
 // /********* Uncomment these lines to test your work! *********/
-// console.log(mike.name); // -> Logs 'Mike'
-// console.log(mike.age); //-> Logs 30
-// mike.greet(); //-> Logs 'hello'
+console.log(mike.name); // -> Logs 'Mike'
+console.log(mike.age); //-> Logs 30
+mike.greet(); //-> Logs 'hello'
 
-/*** CHALLENGE 3 of 3 ***/
-// add code here
+/*** CHALLENGE 3 of 3 **/
 
-// mike.introduce(); // -> Logs 'Hi, my name is Mike'
+mike.introduce(); // -> Logs 'Hi, my name is Mike'
 
 /****************************************************************
                         USING ES6 CLASSES
@@ -91,25 +100,33 @@ var mike = personFromConstructor("Mike", 30);
 /*** CHALLENGE 1 of 3 ***/
 
 class PersonClass {
-  constructor() {
-    // add code here
+  constructor(name) {
+    this.name = name;
   }
-
-  // add code here
+  greet() {
+    console.log("hello");
+  }
 }
 
 // /********* Uncomment this line to test your work! *********/
 var george = new PersonClass();
-// george.greet(); // -> Logs 'hello'
+george.greet(); // -> Logs 'hello'
 
 /*** CHALLENGE 2 of 3 ***/
 
-// add code here
+class DeveloperClass extends PersonClass {
+  constructor(name) {
+    super(name);
+  }
+  introduce() {
+    console.log(`Hi, my name is ${this.name}`);
+  }
+}
 
 // /********* Uncomment these lines to test your work! *********/
-// var thai = new DeveloperClass('Thai', 32);
-// console.log(thai.name); // -> Logs 'Thai'
-// thai.introduce(); //-> Logs 'Hello World, my name is Thai'
+var thai = new DeveloperClass("Thai", 32);
+console.log(thai.name); // -> Logs 'Thai'
+thai.introduce(); //-> Logs 'Hello World, my name is Thai'
 
 /****************************************************************
                       EXTENSION: SUBCLASSING
@@ -129,16 +146,23 @@ function userFactory(name, score) {
   return user;
 }
 
-var adminFunctionStore /* Put code here */;
+var adminFunctionStore = Object.create(userFunctionStore);
 
 function adminFactory(name, score) {
-  // Put code here
+  let obj = userFactory(name, score);
+  Object.setPrototypeOf(obj, adminFunctionStore);
+  obj.type = "Admin";
+  return obj;
 }
+
+adminFunctionStore.sharePublicMessage = function () {
+  console.log(`Welcome users!`);
+};
 
 /* Put code here for a method called sharePublicMessage*/
 
 var adminFromFactory = adminFactory("Eva", 5);
 
 // /********* Uncomment these lines to test your work! *********/
-// adminFromFactory.sayType() // -> Logs "I am a Admin"
-// adminFromFactory.sharePublicMessage() // -> Logs "Welcome users!"
+adminFromFactory.sayType(); // -> Logs "I am a Admin"
+adminFromFactory.sharePublicMessage(); // -> Logs "Welcome users!"
