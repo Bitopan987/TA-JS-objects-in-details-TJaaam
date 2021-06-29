c466c3402981a9be01d37477bb8a8a28bbf568ae
bitopan987 <bitopanxon1@gmail.com>
2021-06-29 23:40:16 +0530

    Refactor

--- block-BJaaim/code/index.js
+++ block-BJaaim/code/index.js
@@ -1,15 +1,23 @@
+let form = document.querySelector("form");
+let prev = document.querySelector(".prev");
+let next = document.querySelector(".next");
+let quizElm = document.querySelector(".quiz");
+
 class Question {
-  constructor(title, options, correctAnswer) {
+  constructor(title, options, correctAnswerIndex) {
     this.title = title;
     this.options = options;
-    this.correctAnswer = correctAnswer;
+    this.correctAnswerIndex = correctAnswerIndex;
   }
+
   isCorrect(answer) {
-    return this.correctAnswer === answer;
+    return this.options[this.correctAnswerIndex] === answer;
   }
+
   getCorrectAnswer() {
-    return this.correctAnswer;
+    return this.options[this.correctAnswerIndex];
   }
+
   createUI() {
     let li = document.createElement("li");
     let input = document.createElement("input");
@@ -26,29 +34,69 @@ class Question {
 }
 
 class Quiz {
-  constructor(allQuestions = [], activeIndex, score) {
-    this.questions = allQuestions;
-    this.activeIndex = activeIndex;
+  constructor(questions = [], score = 0) {
+    this.questions = questions;
     this.score = score;
-
+    this.activeIndex = 3;
   }
-  
-  nextQuestion(){
 
+  nextQuestion() {
+    this.activeIndex = this.activeIndex + 1;
+    this.createUI();
   }
+
+  prevQuestion() {
+    this.activeIndex = this.activeIndex - 1;
+    this.createUI();
+  }
+
+  addQuestion(title, options, AnswerIndex) {
+    let question = new Question(title, options, AnswerIndex);
+    this.questions.push(question);
+  }
+
   createUI() {
-    this.root.innerText = "";
-    this.todos.forEach((todo) => {
-      let ui = todo.createUI();
-      ui.querySelector("span").addEventListener(
-        "click",
-        this.handleDelete.bind(this, todo.id)
-      );
-      this.root.append(ui);
-    });
+    quizElm.innerText = "";
+    let activeQuestion = this.questions[this.activeIndex];
+    let form = document.createElement("form");
+    let fieldset = document.createElement("fieldset");
+    let legend = document.createElement("legend");
+    legend.innerText = activeQuestion.title;
+    let optionElm = document.createElement("div");
+    optionElm.classList.add("options");
+    let buttonWrapper = document.createElement("div");
+    let button = document.createElement("button");
+    button.classList.add("btn-submit");
+    button.type = "submit";
+    button.innerText = "Submit";
+    buttonWrapper.append(button);
 
-    updateScore(){
-     
-    }
+    activeQuestion.options.forEach((option, index) => {
+      let input = document.createElement("input");
+      let div = document.createElement("div");
+      input.id = `option-${index}`;
+      input.type = "radio";
+      input.name = "options";
+      input.value = option;
+      let label = document.createElement("label");
+      label.for = `option-${index}`;
+      label.innerText = option;
+      div.append(input, label);
+      optionElm.append(div);
+    });
+    fieldset.append(legend, optionElm, buttonWrapper);
+    form.append(fieldset);
+    quizElm.append(form);
   }
 }
+
+let quiz = new Quiz();
+
+quizCollection.forEach((question) => {
+  quiz.addQuestion(question.title, question.options, question.AnswerIndex);
+});
+
+quiz.createUI();
+
+next.addEventListener("click", quiz.nextQuestion.bind(quiz));
+prev.addEventListener("click", quiz.prevQuestion.bind(quiz));
